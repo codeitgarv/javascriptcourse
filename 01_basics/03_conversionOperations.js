0bb74081159195a25ef71b9b2d99facc525867dd
codeitgarv <109695905+codeitgarv@users.noreply.github.com>
2024-09-01 23:54:01 +0000

    more operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,13 +1,13 @@
 let score = "33abc"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber); 
+// console.log(valueInNumber); 
 // "33" => gives 33
 //  "33abc" => NaN 
 //  null => 0
@@ -18,7 +18,7 @@ let isLoggedIn = "garv"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 = true , 0 = false
 // "" = false
@@ -29,5 +29,69 @@ console.log(booleanIsLoggedIn);
 let x = 33
 let stringNum = String(x);
 
-console.log(typeof stringNum);
+// console.log(typeof stringNum);
+
+// ************ operations *************
+
+let value = 3
+
+let negValue = -value 
+
+// console.log(negValue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello "
+let str2 = "garv"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log(1 + "2");
+    
+// console.log(1 + 1 + "2");
+
+// console.log("1" + 2 + 2 );
+
+//  everything should be in parenthesis 
+
+// console.log((1 + 2)*25);
+
+
+// **********special operations
+
+
+// console.log(+true); output= 1
+
+// console.log(+""); output = 0
+
+// let num1 , num2 , num3  
+
+// num1= num2= num3 = 2+2
+
+// let gameCounter = 100;
+
+// ++gameCounter;
+
+// console.log(gameCounter);
+
+
+// let a = 3;
+// const b = a++;
+
+// console.log("a,b")
+// // Expected output: "a:4, b:3"
+
+// ++A= constant will change
+// A++ = constant willnot change
+
+
+
 
