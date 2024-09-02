613c6e3607e826c3f337085378c367882d975e79
codeitgarv <109695905+codeitgarv@users.noreply.github.com>
2024-09-02 04:06:03 +0000

    comparison and datatypesummary

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+
+
+// console.log(null > 0);
+
+// console.log(null == 0);
+ 
+// console.log(null >= 0); output of thus is true because of conversion 
+
+//  === is used to check strictly 
---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,30 @@
+// Primitive datatype
+
+// 7 types : String , Boolean , Numbers , null , undefined , Symbol , BigInt
+
+// const score = 100 ;
+// const avgscore = 1101.5;
+// const isLoggedIn = false;
+// const outsideTemp = null;
+// let usermail;
+
+// const Id = Symbol("123");
+// const anotherId = Symbol("123");
+
+// console.log(Id === anotherId);
+
+
+// Reference type : (non primitive)
+
+// Array , Objects , Functions
+
+const heroes = ["naagraj", "shaktimaan" , "doga"];
+let myObj = {
+    name : "garvit" , 
+    age: 22 ,
+};
+
+const myFunction = function(){
+console.log("hello world");
+
+};
