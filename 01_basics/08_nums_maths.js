f1a9c559cc3f62589b5b6f228d0ddb5cd67b11ff
codeitgarv <109695905+codeitgarv@users.noreply.github.com>
2024-09-04 01:16:28 +0000

    nums and maths

---
+++ 01_basics/08_nums_maths.js
@@ -0,0 +1,39 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(1245612);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+// console.log(score.toString().replace('400','500'));
+
+// console.log(balance.toPrecision(3));
+
+// console.log(balance.toLocaleString('en-IN'));
+//  to read big numbers with commas in indian standard 
+
+// +++++++++++++ maths +++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-65));
+// console.log(Math.round(4.6665555));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(4.2,5,4.9,3.8,3.3));
+// console.log(Math.max(4,5,7));
+
+// console.log(Math.random());
+
+const min = 1
+const max = 40
+
+console.log(Math.floor(Math.random() * (max - min + 1 ) ) + min );
+
+//  this is most important in maths 
+
+
+
+
+
