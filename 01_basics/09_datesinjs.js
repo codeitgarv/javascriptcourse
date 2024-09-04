6acb83ad4db27d31a677d1a2b6e154eef7c1e853
codeitgarv <109695905+codeitgarv@users.noreply.github.com>
2024-09-04 02:47:08 +0000

    datesinjs

---
+++ 01_basics/09_datesinjs.js
@@ -0,0 +1,44 @@
+//  DATES
+
+// let myDate = new Date();
+// console.log(myDate.toString());
+
+// console.log(myDate.toDateString());
+
+// console.log(myDate.toISOString());
+
+// console.log(myDate.toJSON());
+
+// console.log(myDate.toLocaleDateString());
+
+// console.log(myDate.toLocaleString());
+
+// console.log(myDate.toTimeString());
+
+// let newDate = new Date(2023,11,3);
+// console.log(newDate.toDateString());
+
+
+let newDate = new Date("2024-01-30");
+// console.log(newDate.toLocaleString());
+
+// let timestamp = Date.now();
+// console.log(timestamp);
+// console.log(newDate.getTime());
+
+// console.log(Math.floor(timestamp/1000));
+
+// console.log(newDate.getMonth());
+
+console.log(newDate.toLocaleString(`default`,{
+    weekday : "short"
+}));
+
+
+
+
+
+
+
+
+
