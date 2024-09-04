9d72e2400ccc193cc530da5881d660fe547b81c8
codeitgarv <109695905+codeitgarv@users.noreply.github.com>
2024-09-04 07:33:20 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,57 @@
+// Arrays
+//  () = parenthesis
+//  [] = brackets
+//  {} = braces
+
+// const myArray = ["hitesh",5,2,6,3]
+// arrays can contain both primitive and non primitive datatypes
+// console.log(myArray[0]);
+// console.log(typeof myArray);
+// console.log(myArray.length);
+
+// array methods 
+
+// myArray.push("bgmi")
+// push is used to add a new data type in the array from last 
+// console.log(myArray);
+
+// myArray.pop()
+// pop is used to remove last data type of an array from last 
+
+// myArray.unshift(20)
+// unshift is used to add a new data type at the starting of an array
+// myArray.shift()
+//  shift is used to remove a data type from starting 
+// console.log(myArray);
+
+// console.log(myArray.includes(6));
+// // console.log(myArray.indexOf("hitesh"));
+
+// const newArray = myArray.join();
+// console.log(newArray);
+// console.log(typeof newArray);
+
+const game1 = [0,1,2,3,4,5];
+console.log("A:", game1);
+
+const nm1 = game1.slice(0,3);
+
+console.log(nm1);
+console.log("B:", game1);
+
+const nm2 = game1.splice(0,4);
+console.log(nm2);
+console.log("C:", game1);
+
+
+
+
+
+
+
+
+
+
+
+
+
